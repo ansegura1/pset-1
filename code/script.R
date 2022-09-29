feda040e974bf54032792f0810636d04e29de611
ansegura1 <111020323+ansegura1@users.noreply.github.com>
2022-09-28 19:28:01 -0500

    Avance 2

--- code/script.R
+++ code/script.R
@@ -19,10 +19,13 @@ vector1 <- c( 1:100) ##Creamos un vector que tiene los numeros del 1 al 100##
 vector2<- seq(1,99, by=2) ##Creamos un vector que tiene los numeros impares del 1 al 99.
 vector3<- vector1[-vector2]##Creamos un vector que tiene los numeros. Se realizo mediante la eleminacion de los los elementos del vector1 que estuvieran en el vector2. 
 
-export(vector1,file="output/vector1.csv")
-
-##2. 
-
+##2. Importar/exportar bases de datos##
+#2.1 Importar
+data_csv = import(file="input/Enero - Cabecera - Caracteristicas generales (Personas).csv")
+data_csv2= import(file ="input/Enero - Cabecera - Ocupados.csv" )
+#2.2 Exportar 
+export(x=data_csv , file="output/Característicasgenerales(Personas).rds")
+export(x=data_csv2 , file="output/Ocupados.rds")
 
 
 
